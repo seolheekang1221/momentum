da270654026fc3aeac6c40b868c4742c6a17bc91
강설희 <gangseolhui@SeolheeKang.local>
2020-09-07 22:59:49 -0400

    implemented todo

--- js/todo.js
+++ js/todo.js
@@ -1,2 +1,40 @@
 const todoForm = document.querySelector(".todo-form");
+const todos = document.querySelector('.todos');
+const todoList = [];
 
+/*
+<li class="item">
+    <input type="checkbox" name="" id="">
+    <span class="todo">Study react and english</span>
+    <span class="update">📝</span>
+    <span class="delete">🗑️</span>
+</li>
+*/
+
+function renderTodo(todo) {
+    return `<li class="item">
+    <input type="checkbox">
+    <span class="todo">${todo}</span>
+    <span class="update">📝</span>
+    <span class="delete">🗑️</span>
+</li>`
+}
+
+function todoSubmitHandler(event) {
+    event.preventDefault();
+    // Todo: firstElementChild vs firstChild
+    const todoValue = this.firstElementChild.value;
+    todoList.push(todoValue);
+    todos.innerHTML += renderTodo(todoValue);
+    this.firstElementChild.value = '';
+}
+
+function main() {
+    todoForm.onsubmit = todoSubmitHandler;
+    todos.innerHTML = ''
+    todoList.forEach(todo => {
+        todos.innerHTML += renderTodo(todo);
+    })
+}
+
+main();
