2c9bf5c41834088932bbd5c6e15fe9e95e437fa3
강설희 <gangseolhui@SeolheeKang.local>
2020-09-11 19:17:48 -0400

    added modal background for login and logout

--- js/user.js
+++ js/user.js
@@ -28,11 +28,13 @@ function clickHandler() {
     loginContainer.hidden = !loginContainer.hidden;
     console.log(loginContainer.hidden, localStorage.getItem(LS_GREET_KEY.NAME))
     console.log(!loginContainer.hidden && localStorage.getItem(LS_GREET_KEY.NAME) !== 'rockstar');
+    document.querySelector('.login-background').style.display = 'block';
     if (!loginContainer.hidden && localStorage.getItem(LS_GREET_KEY.NAME) !== "") {
         loginContainer.innerHTML = `<button id="logout">Logout</button>`;
         const logout = document.querySelector("#logout");
         logout.onclick = function () {
             localStorage.removeItem(LS_GREET_KEY.NAME);
+            document.querySelector('.login-background').style.display = 'none';
             document.location.href = "/";
         }
     }
@@ -43,6 +45,7 @@ function submitHandler(event) {
     event.preventDefault();
     console.log(this.children[0].value);
     localStorage.setItem(LS_GREET_KEY.NAME, this.children[0].value);
+    document.querySelector('.login-background').style.display = 'none';
     document.location.href = "/";
 }
 
